557f1d245716fd2e5748ac507540d780f8208107
Martin Gausseran <martin.gausseran@gmail.com>
2024-08-29 12:21:44 +0200

    Exercice 5 terminé

---
+++ intro/step5.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+//On crée une classe 'beverage'
+class beverage {
+
+    //On lui attribue des propriétés
+    private $color;
+    private $price;
+    private $temperature;
+
+    
+
+    //Dans le constructeur, on définit le type de ces propriétés (l.11) et on les "imprime" sur l'objet à créer (l.14 à 16)
+    public function __construct(string $color, float $price, string $temperature='cold')
+    {
+        $this->color=$color;
+        $this->price=$price;
+        $this->temperature=$temperature;
+    }
+
+    function getInfo()
+    {
+        echo "This beer price is " .$this->price;
+    }
+}
+
+$duvel = new beverage('Duvel', 8.5, 'light', 3.5);
+echo $duvel->getInfo();
