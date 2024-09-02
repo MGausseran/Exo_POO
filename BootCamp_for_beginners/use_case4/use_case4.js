e1b49d9581ed53c9d577e5477dda19401d49fbc5
Martin Gausseran <martin.gausseran@gmail.com>
2024-09-02 16:03:43 +0200

    Création de la classe student et des deux groupes pour l'exo 4

---
+++ BootCamp_for_beginners/use_case4/use_case4.js
@@ -0,0 +1,32 @@
+class student {
+    constructor(name, grade) {
+        this.name=name;
+        this.grade=grade;
+    }
+}
+
+const groupe1 = [
+    new student('Alice', 16),
+    new student('Bob', 18),
+    new student('Charlie', 14),
+    new student('David', 17),
+    new student('Eva', 19),
+    new student('Frank', 12),
+    new student('Grace', 15),
+    new student('Hannah', 13),
+    new student('Ian', 20),
+    new student('Judy', 11),
+];
+
+const groupe2 = [
+    new student('Klaus', 16),
+    new student('Lily', 17),
+    new student('Mona', 18),
+    new student('Nick', 14),
+    new student('Olivia', 19),
+    new student('Paul', 15),
+    new student('Quincy', 13),
+    new student('Rachel', 20),
+    new student('Steve', 12),
+    new student('Tina', 11)
+];
