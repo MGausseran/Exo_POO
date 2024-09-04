fcf9b80f36fa12696ba6c799390c0a20b96ae87a
MGausseran <martin.gausseran@gmail.com>
2024-09-04 12:23:01 +0200

    Classe Word créée

--- BootCamp_intermediate/classes/Word.php
+++ BootCamp_intermediate/classes/Word.php
@@ -1,13 +1,44 @@
 <?php
 
+require 'Data.php';
 class Word
 {
-    // TODO: add word (FR) and answer (EN) - (via constructor or not? why?)
+    private $word;
+    private $answer;
 
-    public function verify(string $answer): bool
+    public function __construct()
     {
-        // TODO: use this function to verify if the provided answer by the user matches the correct one
-        // Bonus: allow answers with different casing (example: both bread or Bread can be correct answers, even though technically it's a different string)
-        // Bonus (hard): can you allow answers with small typo's (max one character different)?
+        //On stocke dans $words le tableau de mots récupéré par l'appel de la méthode words() de la classe Data
+        $words = Data::words();
+        $this->word = array_rand($words); //Array_rand sélectionne au hasard une valeur de clé dans l'array $words
+        $this->answer = $words[$this->word]; //On récupère la traduction en anglais
+    }
+
+    public function getWord(): string //La propriété $word étant private, il faut avoir recours à un Getter pour pouvoir y accèder 
+    {
+        return $this->word;
+    }
+
+    private function marginOfError($proposal, $answer) { //On va calculer une marge d'erreur pour que le jeu tolère une marge d'erreur d'une typo
+
+        $margin = 1;
+        //levenshtein prend en argument deux strings et va déterminer sur base de $margin à partir de cb lettres de différence ces deux strings peuvent être considérées
+        //comme semblables ou différentes
+        $reference = levenshtein($proposal,$answer);
+        //si l'écart levenshtein est égal ou inférieur à une lettre d'erreur, alors on accepte le mot
+        if ($reference <= $margin) {
+            return true;
+        };
+        
+    }
+
+    public function verify(string $proposal): bool //On va vérifier si la proposition du joueur est bonne
+    {   $proposal = strtolower($proposal); //Par souci de praticité, on convertit automatiquement la proposition et la bonne réponse en minuscule pour éviter un conflit de correspondance de strings
+        $answer = strtolower($this->answer);
+        //Si la proposition est STRICTEMENT égale à la bonne réponse, ou qu'elle varie d'une lettre, alors la proposition est validée
+        if ($proposal === $answer || $this->marginOfError($proposal, $answer)) {
+            return true;
+        }
+        return false;
     }
 }
