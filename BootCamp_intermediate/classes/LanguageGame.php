bbf9846c297fc343250cf98a3f7afae2bd3b3a44
Martin Gausseran <martin.gausseran@gmail.com>
2024-09-13 15:03:31 +0200

    Update LanguageGame.php

--- BootCamp_intermediate/classes/LanguageGame.php
+++ BootCamp_intermediate/classes/LanguageGame.php
@@ -6,25 +6,21 @@ class LanguageGame
 {
     public function run(): void
     {
-        // Si aucun joueur n'est défini, demander un pseudo
         if (!isset($_SESSION['player'])) {
             if (isset($_POST['nickname'])) {
                 $_SESSION['player'] = serialize(new Player($_POST['nickname']));
                 $this->initializeGame();
             }
         } else {
-            // Si le jeu a déjà commencé, gérer les réponses
             if (isset($_POST['answer'])) {
                 $this->checkAnswer($_POST['answer']);
             } else {
-                // Générer un nouveau mot si nécessaire
                 if (!isset($_SESSION['current_word'])) {
                     $this->generateNewWord();
                 }
             }
         }
 
-        // Gérer la réinitialisation
         if (isset($_POST['reset'])) {
             $this->resetGame();
         }
@@ -32,7 +28,6 @@ class LanguageGame
 
     private function initializeGame(): void
     {
-        // Générer un nouveau mot
         $this->generateNewWord();
     }
 
@@ -46,8 +41,8 @@ class LanguageGame
 
     private function checkAnswer(string $answer): void
     {
-        $player = unserialize($_SESSION['player']); // Récupérer l'objet Player
-        $currentWord = unserialize($_SESSION['current_word']); // Récupérer le mot courant
+        $player = unserialize($_SESSION['player']); 
+        $currentWord = unserialize($_SESSION['current_word']);
 
         if ($currentWord->verify($answer)) {
             $_SESSION['result_message'] = "Correct! The translation of '" . $currentWord->getWord() . "' is indeed '" . $answer . "'.";
@@ -57,14 +52,13 @@ class LanguageGame
             $player->increaseIncorrect();
         }
 
-        // Enregistrer le joueur mis à jour dans la session
         $_SESSION['player'] = serialize($player);
 
-        // Vérifier si le jeu est terminé
+ 
         if ($player->getScore()['correct'] >= 10 || $player->getScore()['incorrect'] >= 10) {
             $_SESSION['game_over'] = true;
         } else {
-            $this->generateNewWord(); // Générer un nouveau mot
+            $this->generateNewWord(); 
         }
     }
 
